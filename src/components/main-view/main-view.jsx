386576354818a1694b3878e6d19208b1b6dbeef9
Lance ODonnell <lance.odonnell.wd@gmail.com>
2021-12-05 12:23:10 -0700

    Update main-view.jsx
    linked to movie database
    
    removed list empty error code

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -11,15 +11,23 @@ export default class MainView extends React.Component {
   constructor(){
     super(); //initializes components state
     this.state = {
-      movies: [
-        { _id: 1, Title: 'Silence of the Lambs', Description: 'A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer.', ImagePath: imgPath + 'Silence.jpg', Genre:'Thriller', Director:'Jonathan Demme'},
-        { _id: 2, Title: '40 Year Old Virgin', Description: 'Goaded by his buddies, a nerdy guy whos never done the deed only finds the pressure mounting when he meets a single mother.', ImagePath: imgPath + '40Year.jpg', Genre:'Comedy', Director:'Judd Apatow'},
-        { _id: 3, Title: 'Catch Me If You Can', Description: 'Barely 21 yet, Frank is a skilled forger who has passed as a doctor, lawyer and pilot. FBI agent Carl becomes obsessed with tracking down the con man, who only revels in the pursuit.', ImagePath: imgPath + 'CatchMe.jpg', Genre:'Biography', Director:'Steven Spielberg'}
-      ],
+      movies: [],
       selectedMovie: null
     }
   }
 
+  componentDidMount(){
+    axios.get('https://mymoviesproject.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  
   setSelectedMovie(newSelectedMovie) {
     this.setState({
       selectedMovie: newSelectedMovie
@@ -29,7 +37,7 @@ export default class MainView extends React.Component {
   render() {
     const { movies, selectedMovie } = this.state;
   
-    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+    if (movies.length === 0) return <div className="main-view" />;
   
     return (
       <div className="main-view">
