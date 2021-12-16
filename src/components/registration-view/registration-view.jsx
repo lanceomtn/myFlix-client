3210f71dea8dc885f5ef72fba5b1516066978c7e
Lance ODonnell <lance.odonnell.wd@gmail.com>
2021-12-16 15:48:28 -0700

    created registration view

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { Navbar, Nav, Card, Container, Form } from 'react-bootstrap';
 import axios from 'axios';
 import { BrowserRouter as Router, } from 'react-router-dom';
+import { NavbarView } from "../navbar-view/navbar-view";
 
 import './registration-view.scss';
 import '../button/button.scss';
@@ -12,11 +13,16 @@ export function RegistrationView(props) {
   const [ password, setPassword ] = useState('');
   const [ email, setEmail ] = useState('');
   const [ birthday, setBirthday] = useState('');
-
+  const [values, setValues] = useState({
+    usernameErr: '',
+    passwordErr: '',
+    emailErr: '',
+  });
+}
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, name, password, email, birthday);
+    console.log(username, password, email, birthday);
     axios.post('https://https://mymoviesproject.herokuapp.com/user', {
       Username: username,
       Password: password,
@@ -35,40 +41,32 @@ export function RegistrationView(props) {
 
   return (
     <Router>
-    
-    <Container fluid className="register-container">
-    
-      <Navbar className="main-navbar" expand="lg">
-        <Container fluid>
-          <Navbar.Brand href="#home">My Movies</Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link href="#logout">Logout</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>     
-
+      <NavbarView/>
+      
       <Card className="register-card">
         <Card.Body>
           <Card.Title className="text-center">Welcome to My Movies</Card.Title>
           <Card.Subtitle className="mb-2 text-muted text-center">Please Register</Card.Subtitle>
+        </Card.Body>
+      </Card>
         
-          <Form>
-            <Form.Group>
+        <Form>
+          <Form.Group controlId="registerUsername">
               <Form.Label>Username</Form.Label>
-              <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} />
+              <Form.Control type="text" value={username} placeholder="Create Username" onChange={e => setUsername(e.target.value)} />
             </Form.Group>
 
-            <Form.Group>
+            <Form.Group controlId="registerPassword">
               <Form.Label>Password</Form.Label>
               <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} />
             </Form.Group>
 
-            <Form.Group>
+            <Form.Group controlId="registerEmail">
               <Form.Label>Email</Form.Label>
               <Form.Control type="email" value={email} placeholder="Enter email" onChange={e => setEmail(e.target.value)} />
             </Form.Group>
 
-            <Form.Group>
+            <Form.Group controlId="registerBirthday">
               <Form.Label>Birthday</Form.Label>
               <Form.Control className="mb-3" type="date" value={birthday} onChange={e => setBirthday(e.target.value)} />
             </Form.Group>
@@ -79,19 +77,14 @@ export function RegistrationView(props) {
             </button>
           </div>
         </Form>  
-        </Card.Body>
-      </Card>
-    </Container> 
-
     </Router> 
   );
-}
 
 RegistrationView.propTypes = {
   register: PropTypes.shape({
     username: PropTypes.string.isRequired,
     password: PropTypes.string.isRequired,
-    email: PropTypes.string.isRequired,
-    birthday: PropTypes.string.isRequired
+    email: PropTypes.string.isRequired
+    //birthday: PropTypes.string.isRequired
   })
 };
