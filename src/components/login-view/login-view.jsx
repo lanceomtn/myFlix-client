c445397cbb0e99fac0caf5772718bb71541918b4
Lance ODonnell <lance.odonnell.wd@gmail.com>
2021-12-16 15:47:41 -0700

    exercise 3.6

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Navbar, Nav, Form, Button, Container } from 'react-bootstrap';
 import axios from 'axios';
+import { NavbarView } from "../navbar-view/navbar-view"; 
 
 import './login-view.scss';
 import '../button/button.scss';
@@ -10,44 +11,69 @@ import { BrowserRouter as Router } from 'react-router-dom';
 export function LoginView(props) {
   const [ username, setUsername ] = useState('');
   const [ password, setPassword ] = useState('');
+  // Declare hook for each input
+  const [ usernameErr, setUsernameErr ] = useState('');
+  const [ passwordErr, setPasswordErr ] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    /* Send a request to the server for authentication */
+// validate user inputs
+const validate = () => {
+  let isReq = true;
+  if(!username){
+   setUsernameErr('Username Required');
+   isReq = false;
+  }else if(username.length < 2){
+   setUsernameErr('Username must be 2 characters long');
+   isReq = false;
+  }
+  if(!password){
+   setPasswordErr('Password Required');
+   isReq = false;
+  }else if(password.length < 6){
+   setPassword('Password must be 6 characters long');
+   isReq = false;
+  }
+
+  return isReq;
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const isReq = validate();
+  if(isReq) {
+    /* Send request to the server for authentication */
     axios.post('https://mymoviesproject.herokuapp.com/login', {
-      Username: username,
-      Password: password
+        Username: username,
+        Password: password
     })
-    .then(response => {
-      const data = response.data;
-      props.onLoggedIn(data);
+    .then(response =>{
+        const data = response.data;
+        props.onLoggedIn(data);
     })
     .catch(e => {
       console.log('no such user')
     });
-  };
+  }
+};
 
   return (
+    
     <Router>
 
-     <Navbar className="main-navbar" expand="lg">
-      <Container fluid>
-        <Navbar.Brand href="#home">My Movies</Navbar.Brand>
-         <Nav className="me-auto">
-          <Nav.Link href="#logout">Logout</Nav.Link>
-         </Nav>
-        </Container>
-      </Navbar>     
+     <NavbarView/>   
     
       <Form className="login-form">
         <Form.Group controlId="formUsername">
           <Form.Label>Username:</Form.Label>
           <Form.Control type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} />
+          {/* code added here to display validation error */}
+          {usernameErr && <p>{usernameErr}</p>} 
         </Form.Group>
 
         <Form.Group controlId="formPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+          {/* code added here to display validation error */}
+          {passwordErr && <p>{passwordErr}</p>} 
         </Form.Group>
 
         <div className="submit-button-div"></div>
