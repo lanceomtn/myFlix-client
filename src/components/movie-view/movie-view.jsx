390355effb75f55192d4593e2121b28902e2b603
Lance ODonnell <lance.odonnell.wd@gmail.com>
2021-12-19 17:36:25 -0700

    Update movie-view.jsx
    added navbar view.  minor style changes

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import { Button, Container, } from 'react-bootstrap';
 import { Link } from "react-router-dom";
+import { NavbarView } from "../navbar-view/navbar-view";
 
 import './movie-view.scss';
 import '../button/button.scss'
@@ -10,61 +11,63 @@ export class MovieView extends React.Component {
   
   constructor(props) {
     super(props);
-}
+  }
 
-addFavoriteMovie() {
-  const token = localStorage.getItem('token');
-  const username = localStorage.getItem('user');
+  addFavoriteMovie() {
+    const token = localStorage.getItem('token');
+    const username = localStorage.getItem('user');
 
-  axios.post(`https://mymoviesproject.herokuapp.com/user/favorites/${username}/movies/${this.props.movie._id}`, {}, {
+    axios.post(`https://mymoviesproject.herokuapp.com/users/favorites/${username}/movies/${this.props.movie._id}`, {}, {
       headers: { Authorization: `Bearer ${token}` },
       method: 'POST'
-  })
+    })
       .then(response => {
           alert(`Added to Favorites List`)
       })
       .catch(function (error) {
           console.log(error);
       });
-};
+  }; 
 
   render() {
     const { movie, onBackClick } = this.props;
+
+    <NavbarView user={user}/>
     
     return (
-      <Container className="movie-view-container">
-        <div className="movie-poster">
-          <img src={movie.ImagePath}/>
-        </div>
-        <div className="movie-title">
-          <span className="title">Title: </span>
-          <span className="value">{movie.Title}</span>
-        </div>
-        <div className="movie-genre">
-          <span className="genre">Genre: </span>
-          <Link to={`/genres/${movie.Genre.Name}`}>
-            <Button variant="link">Genre</Button>
-          </Link>
-        </div>
-        <div className="movie-description">
-          <span className="description">Description: </span>
-          <span className="value">{movie.Description}</span>
-        </div>
-        <div className="movie-director">
-          <span className="director">Director: </span>
-          <Link to={`/directors/${movie.Director.Name}`}>
-            <Button variant="link">Director</Button>
-          </Link>
-        </div>
-        <div className="movie-year">
-          <span className="year">Year: </span>
-          <span className="value">{movie.Year}</span>
-        </div>
-        <div className="movie-button-div">
-          <Button className="submit-button" onClick={() => { onBackClick(null); }}>Back</Button>
-          <Button className="submit-button" value={movie._id} onClick={(e) => this.addFavoriteMovie(e, movie)}>Add to Favorites</Button>
-        </div>
-      </Container>    
+      <Container fluid className="movie-view-container" align="center">
+          <div className="movie-poster">
+            <img src={movie.ImagePath}/>
+          </div>
+          <div className="movie-title">
+            <span className="title">Title: </span>
+            <span className="value">{movie.Title}</span>
+          </div>
+          <div className="movie-genre">
+            <span className="genre">Genre: </span>
+            <Link to={`/genres/${movie.Genre.Name}`}>
+              <Button variant="link">Genre</Button>
+            </Link>
+          </div>
+          <div className="movie-description">
+            <span className="description">Description: </span>
+            <span className="value">{movie.Description}</span>
+          </div>
+          <div className="movie-director">
+            <span className="director">Director: </span>
+            <Link to={`/directors/${movie.Director.Name}`}>
+              <Button variant="link">Director</Button>
+            </Link>
+          </div>
+          <div className="movie-year">
+            <span className="year">Year: </span>
+            <span className="value">{movie.Year}</span>
+          </div>
+          <div className="movie-button-div">
+            <Button className="submit-button" onClick={() => { onBackClick(null); }}>Back</Button>
+            <Button className="submit-button" value={movie._id} onClick={(e) => this.addFavoriteMovie(e, movie)}>Add to Favorites</Button>
+          </div>
+        </Container>    
     );
   }
 } 
