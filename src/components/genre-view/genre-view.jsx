bf2d99dbf40a287655f09daeeb7ee8685afa7ab1
Lance ODonnell <lance.odonnell.wd@gmail.com>
2022-01-03 20:01:45 -0700

    fixed routing

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -8,17 +8,17 @@ import './genre-view.scss';
 
 export function GenreView (props) {
   const { Genre, onBackClick } = props
-  
+  console.log('movies',props)
   return (
     <Row className="genre-view">
       <Col>
         <div className="movie-genre">
           <span className="label">Genre: </span>
-          <span className="value">{Genre.Name}</span>
+          <span className="value">{Genre.Genre.Name}</span>
         </div>
         <div className="genre-description">
           <span className="label">Description: </span>
-          <span className="value">{Genre.Description}</span>
+          <span className="value">{Genre.Genre.Description}</span>
         </div>
     
         <Button className="submit-button" onClick={() => { onBackClick(null); }}>Back</Button>
